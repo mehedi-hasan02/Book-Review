26b92eab63e9f627802313c7903e3d1f5b4d5906
mehedi-hasan02 <mehedisani02@gmail.com>
2024-03-26 19:02:59 +0600

    make shape-chart responsive

--- src/Pages/PagesToRead.jsx
+++ src/Pages/PagesToRead.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 import { getBooks } from '../Utility';
 
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
@@ -11,7 +11,7 @@ const PagesToRead = () => {
         const storeBook = getBooks();
         setReadBooks(storeBook);
     }, [])
- 
+
 
     const getPath = (x, y, width, height) => {
         return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
@@ -27,26 +27,28 @@ const PagesToRead = () => {
     };
 
     return (
-        <BarChart
-            width={1200}
-            height={500}
-            data={readBooks}
-            margin={{
-                top: 20,
-                right: 30,
-                left: 20,
-                bottom: 5,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="bookName" />
-            <YAxis />
-            <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-                {readBooks.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                ))}
-            </Bar>
-        </BarChart>
+        <ResponsiveContainer width={`100%`} height={500}>
+            <BarChart
+                // width={1200}
+                // height={500}
+                data={readBooks}
+                margin={{
+                    top: 20,
+                    right: 50,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="bookName" />
+                <YAxis />
+                <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                    {readBooks.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                    ))}
+                </Bar>
+            </BarChart>
+        </ResponsiveContainer>
     );
 };
 
